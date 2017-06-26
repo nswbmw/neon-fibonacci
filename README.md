142d60835956454560c25a631f3df44be631398b
nswbmw <gxqzk@126.com>
2017-06-26 11:38:55 +0800

    update

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 A fibonacci demo of [Neon](https://github.com/neon-bindings/neon).
 
+### Environment
+
+```
+rustc -V => rustc 1.20.0-nightly (229d0d326 2017-06-23)
+node -v => v8.1.1
+```
+
 ```js
 const rust = require('./native');
 
@@ -27,7 +34,8 @@ print
 
 ```
 102334155
-node: 964ms
+node: 1006.893ms
 102334155
-rust: 279ms
+rust: 221.644ms
 ```
+
--- native/src/lib.rs
+++ native/src/lib.rs
@@ -6,21 +6,20 @@ use neon::mem::Handle;
 use neon::js::JsInteger;
 
 fn fib(call: Call) -> JsResult<JsInteger> {
-    let scope = call.scope;
-    let index: Handle<JsInteger> = try!(try!(call.arguments.require(scope, 0)).check::<JsInteger>());
-    let index: i32 = index.value() as i32;
-    let result: i32 = fibonacci(index);
-    Ok(JsInteger::new(scope, result))
+  let scope = call.scope;
+  let index: Handle<JsInteger> = try!(try!(call.arguments.require(scope, 0)).check::<JsInteger>());
+  let index: i32 = index.value() as i32;
+  let result: i32 = fibonacci(index);
+  Ok(JsInteger::new(scope, result))
 }
 
 fn fibonacci(n: i32) -> i32 {
-    if n == 1i32 || n == 2i32 {
-      1i32
-    } else {
-      fibonacci(n - 1) + fibonacci(n - 2)
-    }
+  match n {
+    1 | 2 => 1,
+    _ => fibonacci(n - 1) + fibonacci(n - 2)
+  }
 }
 
 register_module!(m, {
-    m.export("fib", fib)
+  m.export("fib", fib)
 });
